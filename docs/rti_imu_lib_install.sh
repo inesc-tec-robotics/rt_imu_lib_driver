e4c9d599f409bdbcb09a1da700badc78d74bece5
Carlos Costa <carloscosta.cmcc@gmail.com>
2015-05-25 09:06:37 +0100

    Added topics for PoseStamped, FluidPressure, Temperature and RelativeHumidity.
    Added .ini path finder using rospkg.
    Added installation script for RTIMULib.

---
+++ docs/rti_imu_lib_install.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+install_dependencies=${1:-true}
+clone_git_repository=${2:-true}
+make_and_build_rtimulib=${3:-true}
+library_path=${4:-~/}
+
+
+
+if [ "${install_dependencies}" = true ]; then
+   echo "\n\n"
+   echo "####################################################################################################"
+   echo "##### Installing dependencies for RTIMULib"
+   echo "####################################################################################################"
+
+   sudo apt-get install cmake
+   sudo apt-get install libqt4-dev
+   sudo apt-get install python-dev
+   sudo apt-get install octave
+   sudo apt-get install i2c-tools
+
+   #sudo raspi-config
+   #sudo adduser pi i2c
+   #i2cdetect -y 1
+fi
+
+
+
+if [ "${clone_git_repository}" = true ]; then
+   echo "\n\n"
+   echo "####################################################################################################"
+   echo "##### Cloning git RTIMULib repository"
+   echo "####################################################################################################"
+
+   cd ${library_path}
+   git clone https://github.com/richards-tech/RTIMULib.git
+
+   echo "\n\n"
+   echo "----------------------------------------------------------------------------------------------------"
+   echo ">>>>> Cloning finished"
+   echo ">>>>> For updating git repository use: git pull"
+   echo "----------------------------------------------------------------------------------------------------"
+fi
+
+
+
+if [ "${make_and_build_rtimulib}" = true ]; then
+   echo "\n\n"
+   echo "####################################################################################################"
+   echo "##### Building RTIMULib"
+   echo "####################################################################################################"
+
+   mkdir -p ${library_path}RTIMULib/Linux/build
+   cd ${library_path}RTIMULib/Linux/build
+   cmake ..
+   make
+
+   echo "\n\n"
+   echo "----------------------------------------------------------------------------------------------------"
+   echo ">>>>> Build of RTIMULib finished"
+   echo "----------------------------------------------------------------------------------------------------"
+
+
+
+   echo "\n\n"
+   echo "####################################################################################################"
+   echo "##### Installing RTIMULib"
+   echo "####################################################################################################"
+
+   sudo make install
+   sudo ldconfig  
+
+   cd ${library_path}RTIMULib/Linux/python/
+   python setup.py build
+   sudo python setup.py install
+
+   echo "\n\n"
+   echo "----------------------------------------------------------------------------------------------------"
+   echo ">>>>> Install of RTIMULib finished"
+   echo "----------------------------------------------------------------------------------------------------"
+   echo "\n\n"
+fi
--- scripts/rt_imu_lib_driver.py
+++ scripts/rt_imu_lib_driver.py
@@ -4,40 +4,67 @@ import RTIMU
 import rospy
 import sys
 import os
+import rospkg
 
 from tf import transformations
 from sensor_msgs.msg import Imu
 from sensor_msgs.msg import MagneticField
+from sensor_msgs.msg import FluidPressure
+from sensor_msgs.msg import Temperature
+from sensor_msgs.msg import RelativeHumidity
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Float64
+
 
 
 def imu_driver():
+    ##############################################################################################################
     # node initialization
+    ##############################################################################################################
     rospy.init_node('rt_imu_lib_driver', anonymous=True)
+    rospack = rospkg.RosPack()
 
 
+    ##############################################################################################################
     # parameters
-    settings_folder = rospy.get_param('~settings_folder', os.getcwd()) # .ini configuration folder
-    settings_filename = rospy.get_param('~settings_filename', 'RTIMULib') # .ini configuration file
+    ##############################################################################################################
+    settings_folder = rospy.get_param('~settings_folder', rospack.get_path('rt_imu_lib_driver') + '/scripts') # .ini configuration folder
+    settings_filename = rospy.get_param('~settings_filename', 'RTIMULib') # .ini configuration file name
     msgs_frame_id = rospy.get_param('~msgs_frame_id', 'imu_link')
-    publish_topic_imu_pose = rospy.get_param('~publish_topic_imu_pose', 'imu_pose') # if empty no message will be published
-    publish_topic_imu = rospy.get_param('~publish_topic_imu', 'imu') # if empty no message will be published
-    publish_topic_magnetic_field = rospy.get_param('~publish_topic_magnetic_field', "magnetic_field") # if empty no message will be published
+    publish_topic_imu_pose = rospy.get_param('~publish_topic_imu_pose', 'imu/pose') # if empty no message will be published
+    publish_topic_imu_pose_with_covariance = rospy.get_param('~publish_topic_imu_pose_with_covariance', 'imu/pose_with_covariance') # if empty no message will be published
+    publish_topic_imu = rospy.get_param('~publish_topic_imu', 'imu/imu') # if empty no message will be published
+    publish_topic_magnetic_field = rospy.get_param('~publish_topic_magnetic_field', "imu/magnetic_field") # if empty no message will be published
+    publish_topic_pressure = rospy.get_param('~publish_topic_pressure', "imu/pressure") # if empty no message will be published
+    publish_topic_temperature = rospy.get_param('~publish_topic_temperature', "imu/temperature") # if empty no message will be published
+    publish_topic_humidity = rospy.get_param('~publish_topic_humidity', "imu/humidity") # if empty no message will be published
     imu_orientation_covariance = rospy.get_param('~imu_orientation_covariance', 0.0005)
     imu_angular_velocity_covariance = rospy.get_param('~imu_angular_velocity_covariance', 0.001)
     imu_linear_acceleration_covariance = rospy.get_param('~imu_linear_acceleration_covariance', 0.0005)
     magnetic_field_covariance = rospy.get_param('~magnetic_field_covariance', 0.01)
+    pressure_variance = rospy.get_param('~pressure_variance', 0.0)
+    temperature_variance = rospy.get_param('~temperature_variance', 0.0)
+    humidity_variance = rospy.get_param('~humidity_variance', 0.0)
     gravity_acceleration = rospy.get_param('~gravity_acceleration', 9.80665)
     msg_header_use_trimulib_time = rospy.get_param('~msg_header_use_trimulib_time', True) # False uses rospy.Time.now()
 
 
+    ##############################################################################################################
     # published topics
-    publisher_pose = rospy.Publisher(publish_topic_imu_pose, PoseWithCovarianceStamped, queue_size=10)
+    ##############################################################################################################
+    publisher_pose = rospy.Publisher(publish_topic_imu_pose, PoseStamped, queue_size=10)
+    publisher_pose_with_covariance = rospy.Publisher(publish_topic_imu_pose_with_covariance, PoseWithCovarianceStamped, queue_size=10)
     publisher_imu = rospy.Publisher(publish_topic_imu, Imu, queue_size=10)
     publisher_magnetic_field = rospy.Publisher(publish_topic_magnetic_field, MagneticField, queue_size=10)
+    publisher_pressure = rospy.Publisher(publish_topic_pressure, FluidPressure, queue_size=10)
+    publisher_temperature = rospy.Publisher(publish_topic_temperature, Temperature, queue_size=10)
+    publisher_humidity = rospy.Publisher(publish_topic_humidity, RelativeHumidity, queue_size=10)
 
 
+    ##############################################################################################################
     # driver initialization
+    ##############################################################################################################
     driver_settings_file = settings_folder + '/' + settings_filename
     if os.path.exists(driver_settings_file + ".ini"):
         rospy.loginfo("Using IMU configuration file %s.ini", driver_settings_file)
@@ -55,21 +82,31 @@ def imu_driver():
     else:
         rospy.logdebug("IMU initialization successful")
 
+    imu_driver.setGyroEnable(True)
+    imu_driver.setAccelEnable(True)
+    imu_driver.setCompassEnable(True)
+
     poll_interval = imu_driver.IMUGetPollInterval()
     rate = 1000 / poll_interval
     rospy.loginfo("IMU polling interval: %s ms | rate: %s hz", poll_interval, rate)
 
 
+    ##############################################################################################################
     # constant message fields setup
+    ##############################################################################################################
     if publish_topic_imu_pose:
-        pose_msg = PoseWithCovarianceStamped()
+        pose_msg = PoseStamped()
         pose_msg.header.frame_id = msgs_frame_id
-        pose_msg.pose.covariance[0] = 1e6 # ignore position
-        pose_msg.pose.covariance[7] = 1e6 # ignore position
-        pose_msg.pose.covariance[14] = 1e6 # ignore position
-        pose_msg.pose.covariance[21] = imu_orientation_covariance
-        pose_msg.pose.covariance[28] = imu_orientation_covariance
-        pose_msg.pose.covariance[35] = imu_orientation_covariance
+    
+    if publish_topic_imu_pose_with_covariance:
+        pose_with_covariance_msg = PoseWithCovarianceStamped()
+        pose_with_covariance_msg.header.frame_id = msgs_frame_id
+        pose_with_covariance_msg.pose.covariance[0] = 1e6 # ignore position
+        pose_with_covariance_msg.pose.covariance[7] = 1e6 # ignore position
+        pose_with_covariance_msg.pose.covariance[14] = 1e6 # ignore position
+        pose_with_covariance_msg.pose.covariance[21] = imu_orientation_covariance
+        pose_with_covariance_msg.pose.covariance[28] = imu_orientation_covariance
+        pose_with_covariance_msg.pose.covariance[35] = imu_orientation_covariance
 
     if publish_topic_imu:
         imu_msg = Imu()
@@ -91,7 +128,25 @@ def imu_driver():
         magnetic_field_msg.magnetic_field_covariance[4] = magnetic_field_covariance
         magnetic_field_msg.magnetic_field_covariance[8] = magnetic_field_covariance
 
+    if publish_topic_pressure:
+        pressure_msg = FluidPressure()
+        pressure_msg.header.frame_id = msgs_frame_id
+        pressure_msg.variance = pressure_variance
+    
+    if publish_topic_temperature:
+        temperature_msg = Temperature()
+        temperature_msg.header.frame_id = msgs_frame_id
+        temperature_msg.variance = temperature_variance
+        
+    if publish_topic_humidity:
+        humidity_msg = RelativeHumidity()
+        humidity_msg.header.frame_id = msgs_frame_id
+        humidity_msg.variance = humidity_variance
+
+
+    ##############################################################################################################
     # imu msg publishing
+    ##############################################################################################################
     polling_rate = rospy.Rate(rate)
     while not rospy.is_shutdown():
         if imu_driver.IMURead():
@@ -99,62 +154,124 @@ def imu_driver():
             imu_data = imu_driver.getIMUData()
             rospy.logdebug("Time stamp -> Driver: %f | ROS: %f", imu_data["timestamp"] / 1e6, current_time.to_sec())
             
+            
+            ##############################################################################################################
             # msg headers time
+            ##############################################################################################################
             if msg_header_use_trimulib_time:
+                trimulib_time = rospy.Time(imu_data["timestamp"] / 1e6)
                 if publish_topic_imu_pose:
-                    pose_msg.header.stamp = rospy.Time(imu_data["timestamp"] / 1e6)
+                    pose_msg.header.stamp = trimulib_time
+                if publish_topic_imu_pose_with_covariance:
+                    pose_with_covariance_msg.header.stamp = trimulib_time
                 if publish_topic_imu:
-                    imu_msg.header.stamp = rospy.Time(imu_data["timestamp"] / 1e6)
+                    imu_msg.header.stamp = trimulib_time
                 if publish_topic_magnetic_field:
-                    magnetic_field_msg.header.stamp = rospy.Time(imu_data["timestamp"] / 1e6)
+                    magnetic_field_msg.header.stamp = trimulib_time
+                if publish_topic_pressure:
+                    pressure_msg.header.stamp = trimulib_time
+                if publish_topic_temperature:
+                    temperature_msg.header.stamp = trimulib_time
+                if publish_topic_humidity:
+                    humidity_msg.header.stamp = trimulib_time
             else:
                 if publish_topic_imu_pose:
                     pose_msg.header.stamp = current_time
+                if publish_topic_imu_pose_with_covariance:
+                    pose_with_covariance_msg.header.stamp = current_time
                 if publish_topic_imu:
                     imu_msg.header.stamp = current_time
                 if publish_topic_magnetic_field:
                     magnetic_field_msg.header.stamp = current_time
+                if publish_topic_pressure:
+                    pressure_msg.header.stamp = current_time
+                if publish_topic_temperature:
+                    temperature_msg.header.stamp = current_time
+                if publish_topic_humidity:
+                    humidity_msg.header.stamp = current_time
 
 
             ##############################################################################################################
             # pose msg
             ##############################################################################################################
             if publish_topic_imu_pose:
-                pose_msg.pose.pose.orientation.w = imu_data["fusionQPose"][0]
-                pose_msg.pose.pose.orientation.x = imu_data["fusionQPose"][1]
-                pose_msg.pose.pose.orientation.y = imu_data["fusionQPose"][2]
-                pose_msg.pose.pose.orientation.z = imu_data["fusionQPose"][3]
-                publisher_pose.publish(pose_msg)
+                if imu_data["fusionQPoseValid"]:
+                    pose_msg.pose.orientation.w = imu_data["fusionQPose"][0]
+                    pose_msg.pose.orientation.x = imu_data["fusionQPose"][1]
+                    pose_msg.pose.orientation.y = imu_data["fusionQPose"][2]
+                    pose_msg.pose.orientation.z = imu_data["fusionQPose"][3]
+                    publisher_pose.publish(pose_msg)
+
+
+            ##############################################################################################################
+            # pose with covariance msg
+            ##############################################################################################################
+            if publish_topic_imu_pose_with_covariance:
+                if imu_data["fusionQPoseValid"]:
+                    pose_with_covariance_msg.pose.pose.orientation.w = imu_data["fusionQPose"][0]
+                    pose_with_covariance_msg.pose.pose.orientation.x = imu_data["fusionQPose"][1]
+                    pose_with_covariance_msg.pose.pose.orientation.y = imu_data["fusionQPose"][2]
+                    pose_with_covariance_msg.pose.pose.orientation.z = imu_data["fusionQPose"][3]
+                    publisher_pose_with_covariance.publish(pose_with_covariance_msg)
 
 
             ##############################################################################################################
             # imu msg
             ##############################################################################################################
             if publish_topic_imu:
-                imu_msg.orientation.w = imu_data["fusionQPose"][0]
-                imu_msg.orientation.x = imu_data["fusionQPose"][1]
-                imu_msg.orientation.y = imu_data["fusionQPose"][2]
-                imu_msg.orientation.z = imu_data["fusionQPose"][3]
-                # gyro rates in radians/sec
-                imu_msg.angular_velocity.x = imu_data["gyro"][0]
-                imu_msg.angular_velocity.y = imu_data["gyro"][1]
-                imu_msg.angular_velocity.z = imu_data["gyro"][2]
-                # accel data in gs
-                imu_msg.linear_acceleration.x = imu_data["accel"][0] * gravity_acceleration
-                imu_msg.linear_acceleration.y = imu_data["accel"][1] * gravity_acceleration
-                imu_msg.linear_acceleration.z = imu_data["accel"][2] * gravity_acceleration
-                publisher_imu.publish(imu_msg)
+                if imu_data["fusionQPoseValid"] and imu_data["gyroValid"] and imu_data["accelValid"]:
+                    imu_msg.orientation.w = imu_data["fusionQPose"][0]
+                    imu_msg.orientation.x = imu_data["fusionQPose"][1]
+                    imu_msg.orientation.y = imu_data["fusionQPose"][2]
+                    imu_msg.orientation.z = imu_data["fusionQPose"][3]
+                    # gyro data in radians/sec
+                    imu_msg.angular_velocity.x = imu_data["gyro"][0]
+                    imu_msg.angular_velocity.y = imu_data["gyro"][1]
+                    imu_msg.angular_velocity.z = imu_data["gyro"][2]
+                    # accel data in gs
+                    imu_msg.linear_acceleration.x = imu_data["accel"][0] * gravity_acceleration # in m/s^2
+                    imu_msg.linear_acceleration.y = imu_data["accel"][1] * gravity_acceleration # in m/s^2
+                    imu_msg.linear_acceleration.z = imu_data["accel"][2] * gravity_acceleration # in m/s^2
+                    publisher_imu.publish(imu_msg)
 
 
             ##############################################################################################################
             # magnetic field msg
             ##############################################################################################################
-            # compass data in uT
             if publish_topic_magnetic_field:
-                magnetic_field_msg.magnetic_field.x = imu_data["compass"][0] * 1e6
-                magnetic_field_msg.magnetic_field.y = imu_data["compass"][1] * 1e6
-                magnetic_field_msg.magnetic_field.z = imu_data["compass"][2] * 1e6
-                publisher_magnetic_field.publish(magnetic_field_msg)
+                if imu_data["compassValid"]:
+                    # compass data in uT
+                    magnetic_field_msg.magnetic_field.x = imu_data["compass"][0] / 1e6 # in Tesla
+                    magnetic_field_msg.magnetic_field.y = imu_data["compass"][1] / 1e6 # in Tesla
+                    magnetic_field_msg.magnetic_field.z = imu_data["compass"][2] / 1e6 # in Tesla
+                    publisher_magnetic_field.publish(magnetic_field_msg)
+
+
+            ##############################################################################################################
+            # pressure field msg
+            ##############################################################################################################
+            if publish_topic_pressure:
+                if imu_data["pressureValid"]:
+                    pressure_msg.fluid_pressure = imu_data["pressure"] # absolute pressure reading in Pascals
+                    publisher_pressure.publish(pressure_msg)
+
+
+            ##############################################################################################################
+            # temperature field msg
+            ##############################################################################################################
+            if publish_topic_temperature:
+                if imu_data["temperatureValid"]:
+                    temperature_msg.temperature = imu_data["temperature"] # temperature in Degrees Celsius
+                    publisher_temperature.publish(temperature_msg)
+
+
+            ##############################################################################################################
+            # magnetic field msg
+            ##############################################################################################################
+            if publish_topic_humidity:
+                if imu_data["humidityValid"]:
+                    humidity_msg.relative_humidity = imu_data["humidity"] # ratio of partial pressure of water vapor to the saturated vapor pressure at a temperature
+                    publisher_humidity.publish(humidity_msg)
 
         polling_rate.sleep()
 
